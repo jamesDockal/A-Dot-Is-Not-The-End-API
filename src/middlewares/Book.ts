6d142f721e5b5496ff8868c600c2d33b32363dd4
james dockal <jamesistranho@gmail.com>
2021-08-04 20:50:46 -0300

    fix: fix BooksMiddleware method bookCredentials

--- src/middlewares/Book.ts
+++ src/middlewares/Book.ts
@@ -11,6 +11,26 @@ class BooksMiddleware {
     if (!chapters) {
       return res.status(400).json({ error: "No chapters provided!" });
     }
+
+    if (!chapters.length || !Array.isArray(chapters)) {
+      return res
+        .status(400)
+        .json({ error: "The chapters must be an array of objects" });
+    }
+
+    try {
+      chapters.forEach((chapter) => {
+        if (!chapter.title || !chapter.content) {
+          throw new Error(
+            "Each chapter must have the key 'title' and 'content'"
+          );
+        }
+      });
+    } catch (error) {
+      return res.status(400).json({
+        error: error.message,
+      });
+    }
     return next();
   }
 }
