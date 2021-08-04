7ba2a0b9bea850092faf281734b534e68dac56c2
james dockal <jamesistranho@gmail.com>
2021-08-03 23:56:22 -0300

    feat: add UsersController register method creation on token

--- src/controllers/Users.ts
+++ src/controllers/Users.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { getRepository } from "typeorm";
 import UserEntity from "../entities/UserEntity";
 import { hash } from "bcrypt";
+import { sign } from "jsonwebtoken";
 
 class UsersController {
   async register(req: Request, res: Response) {
@@ -15,9 +16,13 @@ class UsersController {
       password_hash,
       email,
     });
-    userRepository.save(user);
 
-    return res.json({ user });
+    await userRepository.save(user);
+
+    const secretKey = process.env.SECRET_KEY || "some_secret_key";
+    const token = await sign(user.id, secretKey);
+
+    return res.json({ user, token });
   }
 }
 
