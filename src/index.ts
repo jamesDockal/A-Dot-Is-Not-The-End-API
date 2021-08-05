f622b11a1834376b76cc7d1e2462426e7ae05f48
james dockal <jamesistranho@gmail.com>
2021-08-04 22:34:01 -0300

    teste

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,12 @@ import UsersRouter from "./routes/users";
 // import "./database";
 import BooksRouter from "./routes/books";
 
+import dotenv from "dotenv";
+dotenv.config({
+  path: ".env.local",
+});
+console.log("PORT", process.env.PORT);
+
 const app = express();
 
 app.use(express.json());
@@ -14,5 +20,5 @@ app.get("/", (req, res) => res.send("ok"));
 // app.use("/books", BooksRouter);
 
 app.listen(process.env.PORT || 3000, () =>
-  console.log(`Server running on port ${process.env.PORT || 3000}`)
+  console.log(`Server Running on port ${process.env.PORT || 3000}`)
 );
