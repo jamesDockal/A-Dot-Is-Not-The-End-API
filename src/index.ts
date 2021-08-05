de40bb60cfb1a437c4d3d5f95aa445f4ee3549bc
james dockal <jamesistranho@gmail.com>
2021-08-04 22:45:44 -0300

    teste

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import express from "express";
-// import UsersRouter from "./routes/users";
+import UsersRouter from "./routes/users";
 
-// import "./database";
-// import BooksRouter from "./routes/books";
+import "./database";
+import BooksRouter from "./routes/books";
 
 import dotenv from "dotenv";
 dotenv.config({
@@ -16,8 +16,8 @@ app.use(express.json());
 
 app.get("/", (req, res) => res.send("ok"));
 
-// app.use("/users", UsersRouter);
-// app.use("/books", BooksRouter);
+app.use("/users", UsersRouter);
+app.use("/books", BooksRouter);
 
 app.listen(process.env.PORT || 3000, () =>
   console.log(`Server Running on port ${process.env.PORT || 3000}`)
